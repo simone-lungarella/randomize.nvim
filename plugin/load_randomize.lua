6b4d61d05da27de2145a0d700c900f91c63213a7
simone-lungarella <simonelungarella@gmail.com>
2024-12-30 17:06:47 +0100

    fix: removed load randomize script

--- plugin/load_randomize.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command("RandomizeBetween", function(opts)
-	require("randomize").generate_random_numbers(opts)
-end, { nargs = 1, range = true })
-
-vim.api.nvim_create_user_command("RandomizeDateBetween", function(opts)
-	require("randomize").generate_random_dates(opts)
-end, { nargs = 1, range = true })
--- scripts/minimal_init.vim
+++ scripts/minimal_init.vim
@@ -2,4 +2,3 @@ set rtp+=.
 set rtp+=../plenary.nvim/
 
 runtime! plugin/plenary.vim
-runtime! plugin/load_randomize.lua
